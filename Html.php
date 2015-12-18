4b62173f97ccd9eca0c6138825d595f8a15f423f
chervand <chervand@gmail.com>
2015-12-18 15:04:57 +0100

    html img array src

--- Html.php
+++ Html.php
@@ -9,7 +9,8 @@ class Html extends \yii\bootstrap\Html
     public static function img($src, $options = [], $encode = false, $webroot = '@webroot')
     {
         if ($encode === true) {
-            $file = Yii::getAlias($webroot . DIRECTORY_SEPARATOR . $src);
+            $file = Yii::getAlias($webroot) . DIRECTORY_SEPARATOR;
+            $file .= is_array($src) ? $src[0] : $src;
             $data = file_get_contents($file);
             if ($data !== false) {
                 $src = 'data:' . mime_content_type($file) . ';base64,' . base64_encode($data);
